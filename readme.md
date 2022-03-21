41bae1006a0c12d77984496194ac2705c982f76b
federicabaczai <federica.baczai@student.h-is.com>
2022-03-21 12:07:15 +0100

    This is mine work. Federica Baczai

---
+++ readme.md
@@ -0,0 +1,7 @@
+14 marzo
+1 - Impostare il progetto node js 
+2 - Installare tutte le dipendenze (npm i express ejs body-parser mongoose) - Collegarsi al database mongo 
+3 - Gestire il file index.js, le rotte e i modelli
+
+Obiettivo ( )
+Creare un'applicazione che memorizzi ed elenchi gli eventi nella scuola H-Farm
---
+++ routes/frontend.js
@@ -0,0 +1,47 @@
+// dependencies 
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Get the models
+const Article = mongoose.model('articles')
+
+
+// create the routes
+const router = express.Router()
+
+// create the request
+//list of the articles
+router.get('/', async (req,res) => {
+  // render the list of articles
+  const articles = await Article.find({}).lean(true)
+  console.log(articles)
+  res.render('homepage', {
+    articles:articles
+  })
+})
+router.post('/articles/create', async (req, res) => {
+  // create  a new document on the database
+  // await Article.create(req.body)
+  const body = req.body;
+  const article = new Article(body)
+  await article.save()
+  // console.log(req.body)
+  res.json(article.toObject());
+})
+
+router.get('/articles/create', async (req,res) => {
+  res.render('new-article')
+})
+
+router.get('/articles/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id).lean(true)
+  // console.log(article)
+  
+  if (article) res.render('article', {
+    article: article
+  })
+  else res.sendStatus(404)
+})
+
+// export the requests
+module.exports = router;
